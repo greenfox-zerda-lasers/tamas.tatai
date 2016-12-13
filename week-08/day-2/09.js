7e5fdf1b24f53bc527df39fd289eb1da1c54afd7
tamastatai <tataitamas@gmail.com>
2016-12-13 17:10:30 +0100

    ready til 9

---
+++ week-08/day-2/09.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Create a function called ready
+//  - Embed this API endpoint as a <script> tag: https://api.github.com/orgs/greenfox-academy/repos?callback=ready
+//  - create the ready function
+//  - itearate through response.data array and console.log the name property
+
+function lobab(obj) {
+  obj.data.forEach(function(value){
+    console.log(value.name);
+  });
+}
